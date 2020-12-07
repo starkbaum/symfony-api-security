fd4829e760a581385595510d160d29a70270c7ac
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-07 15:06:33 +0100

    Chapter 6: on authentication success

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,15 +4,20 @@
 namespace App\Controller;
 
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login", methods={"POST"})
+     * @param IriConverterInterface $iriConverter
+     * @return JsonResponse|Response
      */
-    public function login()
+    public function login(IriConverterInterface $iriConverter)
     {
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
@@ -20,8 +25,12 @@ class SecurityController extends AbstractController
             ], 400);
         }
 
-        return $this->getUser()
-            ? $this->getUser()->getId()
-            : null;
+        //return $this->getUser()
+        //    ? $this->getUser()->getId()
+        //    : null;
+
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromItem($this->getUser()),
+        ]);
     }
 }
