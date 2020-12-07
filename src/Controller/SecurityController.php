cdd23427b40ad1559439bce85716ec16b2c0fec6
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-07 14:12:00 +0100

    Chapter 3: login with json_login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login", methods={"POST"})
+     */
+    public function login()
+    {
+        return $this->getUser()
+            ? $this->getUser()->getId()
+            : null;
+    }
+}
